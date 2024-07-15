5fe72f2731158787054edcb007e9809aa1a75286
cbchenbin <2717668901@qq.com>
2024-07-15 13:29:57 +0800

    test 5

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
+const timeStamp = new Date().getTime();
+
 module.exports = defineConfig({
   transpileDependencies: true,
   pages: {
@@ -23,4 +25,12 @@ module.exports = defineConfig({
       overlay: false
     }
   },
+  configureWebpack: { // webpack 配置
+    output: { // 把应用打包成umd库格式
+         library:'myLibrary', // 输出重构  打包编译后的文件名称  【模块名称.时间戳】
+    filename: `[name].${timeStamp}.js`,
+         libraryTarget:'umd',
+         globalObject:'this',
+       },
+     },
 });
